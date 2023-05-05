84b680dc3097acd0acfce68c8f67abf5e50f4565
Sergio León <sergioleon4004@hotmail.com>
2023-05-04 19:27:19 -0500

    nuevos archivos

---
+++ components/navbar-offline.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="images/icono.png" alt="Bank Food" width="45" height="45">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">¿Qué hacemos?</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">¿Como ayudarnos?</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contáctenos</a>
+        </li>
+      </ul>
+
+      <form class="d-flex" role="search">
+        <button type="button" class="btn btn-bluish-gray me-2">Dona aquí</button>
+        <button type="button" class="btn btn-outline-bluish-gray">Registrese</button>
+      </form>
+    </div>
+  </div>
+</nav>
